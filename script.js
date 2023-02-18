4ba5939b5de77334e09db7c51bf64e654b71b821
neha0407 <48716678+neha0407@users.noreply.github.com>
2023-02-18 23:50:29 +0200

    added var into js

---
+++ script.js
@@ -0,0 +1,6 @@
+function displayTime(){
+    var dateTime = new Date();
+    var hrs = dateTime.getHours();
+    var min = dateTime.getMinutes();
+    var sec = dateTime.getSeconds();
+    var session = document.getElementById('session');
